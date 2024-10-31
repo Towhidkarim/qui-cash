8af423bf9db02171a67d12a78437af7bc3ba5875
Towhidkarim <towhidkarim123@gmail.com>
2024-10-31 11:41:10 +0600

    Fix db config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,11 +4,14 @@ import { config } from 'dotenv';
 config({
   path: '.env',
 });
+let dbUrl;
+if (process.env.NODE_ENV === 'development') dbUrl = process.env.DATABASE_URL;
+else dbUrl = process.env.NEON_DB_URL;
 
 export default defineConfig({
   dialect: 'postgresql',
   schema: './lib/db/schema.ts',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: dbUrl!,
   },
 });
